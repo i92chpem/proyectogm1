bb1d034be7df950b677b949fda35b34144dd94d0
i92chpem <71692168+i92chpem@users.noreply.github.com>
2021-02-09 19:03:29 +0100

    Create visitante.h
    Creación del archivo visitante.h y finalizado, necesaria revision

---
+++ visitante.h
@@ -0,0 +1,20 @@
+#ifndef VISITANTE_H
+#define  VISITANTE_H
+
+#include <iostream>
+#include <string>
+#include "persona.h"
+
+using namespace std;
+
+class Visitante:public Persona
+{
+  private:
+    string requisitosEspeciales_;
+    
+  public:
+    inline void setRequisitos(string r) {requisitosEspeciales_ = r;}
+    inline string getRequisitos() {return requisitosEspeciales_;}
+};
+
+#endif
