7fc4c32fa9d72d0429e745ada50553bbd0bb6cf0
i92chpem <71692168+i92chpem@users.noreply.github.com>
2021-02-09 19:40:14 +0100

    Update persona.h
    Correción de error de compilación, e inclusión del nuevo método esDni

--- persona.h
+++ persona.h
@@ -3,6 +3,9 @@
 
 #include <iostream>
 #include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -13,10 +16,11 @@ class Persona
     unsigned int telefono;
   public:
     Persona();
-    void setInfo(string n, string a, string D, string f, unsigned int t, string e, string d;
+    void setInfo(string n, string a, string D, string f, unsigned int t, string e, string d);
     string getDni();
     void imprimeContacto();
     inline string getNombre_Completo() {return nombre + " " + apellidos;}
+    bool esDni(dni);
     virtual ~Persona(){};
 };
 
