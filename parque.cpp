228daa8bd402d38413d0c3cf3884ad30cdf00d10
i92chpem <71692168+i92chpem@users.noreply.github.com>
2021-02-09 18:55:59 +0100

    Create parque.cpp
    Creación del archivo parque.cpp y finalizado, necesaria revisión del código

---
+++ parque.cpp
@@ -0,0 +1,38 @@
+#include "parque.h"
+#include <iostream>
+#include <string>
+#include "sendero.h"
+#include <list>
+
+using namespace std;
+
+void Parque::setInfo(string u, string l, string p, string f, string r, string n, int s
+{
+  ubicacion = u;
+  localidad = l;
+  provincia = p;
+  fecha = f;
+  reconocimientos = r;
+  nombre = n;
+  superficie = s;
+}
+
+void Parque::printInfo()
+{
+  cout << "\nNombre: " << nombre;
+  
+  cout << "\nUbicacion: " << ubicacion;
+  
+  cout << "\nLocalidad: " << localidad;
+  
+  cout << "\nProvincia: " << provincia;
+  
+  cout << "\nFecha de declaracion: " << fecha;
+  
+  cout << "\nNumero de senderos: " << senderos.size();
+  
+  cout << "\nSuperficie: " << superficie << " ha";
+  
+  cout << "\nReconocimientos: " << reconocimientos;
+  
+}
